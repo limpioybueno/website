5a268863c5f560fa790846067e5ceb72014a259d
Wilmer Llamas <ext_wllamasr@mercadolibre.com>
2020-08-29 16:37:59 -0500

    Create 404 not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {NotFoundComponent} from './components/notfound/notfound.component';
 
 const routes: Routes = [
-  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
+  {path: '', redirectTo: 'auth/login', pathMatch: 'full'},
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
+  declarations: [NotFoundComponent],
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabled'
 })],
